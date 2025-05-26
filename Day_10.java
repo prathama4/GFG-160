5f24467e1b70da26f111c42e229ba899a9662be4
prathama4 <prathamabiswas04@gmail.com>
2025-05-27 00:24:35 +0530

    kadane's algorithm

---
+++ Day_10.java
@@ -0,0 +1,24 @@
+//Day10
+
+public class Day_10{
+    public static int maxSubarraySum(int[] arr) {
+        int maxSum = arr[0];
+        int currentSum = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {2, 3, -8, 7, -1, 2, 3};
+        int[] arr2 = {-2, -4};
+        int[] arr3 = {5, 4, 1, 7, 8};
+        System.out.println(maxSubarraySum(arr1)); // 11
+        System.out.println(maxSubarraySum(arr2)); // -2
+        System.out.println(maxSubarraySum(arr3)); // 25
+    }
+}
